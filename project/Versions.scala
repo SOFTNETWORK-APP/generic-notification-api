b996cf5353fa10f6b3cc9273b504e53c3588b7c7
Stéphane Manciot <stephane.manciot@gmail.com>
2025-04-25 18:12:51 +0300

    prepare release v 0.8.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,9 +4,9 @@ object Versions {
 
   val slf4j = "1.7.36"
 
-  val genericPersistence = "0.7.1"
+  val genericPersistence = "0.7.3"
 
-  val scheduler = "0.7.1"
+  val scheduler = "0.7.3"
 
   val scalatest = "3.2.16"
 }
